4261234ee8619f8d3608e00bc979334d52a4b7da
yyangg2 <rkdeo4104@naver.com>
2023-11-04 18:46:04 +0900

    v3

--- src/main/java/sanhak6/pinwave/api/MentorApiController.java
+++ src/main/java/sanhak6/pinwave/api/MentorApiController.java
@@ -6,16 +6,21 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 import sanhak6.pinwave.domain.Gender;
 import sanhak6.pinwave.domain.Mentor;
+import sanhak6.pinwave.repository.MentorRepository;
 import sanhak6.pinwave.service.MentorService;
 import javax.validation.Valid;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.toList;
 
 
 @RestController
 @RequiredArgsConstructor
 public class MentorApiController {
 
+    private final MentorRepository mentorRepository;
     private final MentorService mentorService;
 
     /**
@@ -48,35 +53,40 @@ public class MentorApiController {
     /**
      * 조회 API: 마이페이지 - 멘토
      */
-//    @GetMapping("/myPage/mentor/{id}")
-//    public GetMentorResponse myPageMentor(@PathVariable("id") Long id, ) {
-//
-//        Mentor findMentor = mentorService.findOne(id);
-//
-//
-//
-//        List<Member> findMembers = memberService.findMembers();
-//
-//
-//        //엔티티 -> DTO 변환
-//        List<MemberDto> collect = findMembers.stream()
-//                .map(m -> new MemberDto(m.getName()))
-//                .collect(Collectors.toList());
-//
-//        return new Result(collect);
-//    }
+    @GetMapping("/myPage/mentor/{id}")
+    public MentorDto myPageMentor(@PathVariable("id") Long id) {
+        Mentor findMentor = mentorRepository.findOne(id);
 
-
-    @Data
-    @AllArgsConstructor
-    static class Result<T> {
-        private T data;
+        return new MentorDto(findMentor);
     }
 
     @Data
-    @AllArgsConstructor
     static class MentorDto {
+        private String field1;
+        private String field2;
+        private String field3;
+        private String job;
+        private Integer career;
         private String name;
+        private Integer count;
+        private Integer mentorRank;
+        private Integer getReviewCount;
+        private Integer doReviewCount;
+        private String introduce;
+
+        public MentorDto(Mentor mentor) {
+            field1 = mentor.getField1();
+            field2 = mentor.getField2();
+            field3 = mentor.getField3();
+            job = mentor.getJob();
+            career = mentor.getCareer();
+            name = mentor.getName();
+            count = mentor.getCount();
+            mentorRank = mentor.getMentorRank();
+            getReviewCount = mentor.getGetReviewCount();
+            doReviewCount = mentor.getDoReviewCount();
+            introduce = mentor.getIntroduce();
+        }
     }
 
     @Data
